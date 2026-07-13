467f6ac61ac427da3ab865ba948faa287f293c16
dongjooon <odobuy1004@naver.com>
2026-07-13 11:14:30 +0900

    db test

---
+++ db.py
@@ -0,0 +1,15 @@
+# app/database.py
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+# .env 파일 로드
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from db import SessionLocal
+from models import User
+from user import get_users
 
 app = FastAPI(
     title="FastAPI Backend",
@@ -6,7 +10,17 @@ app = FastAPI(
     version="1.0.0"
 )
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "MySQL 연결 성공!"}
+
+@app.get("/users")
+def read_users(db: Session = Depends(get_db)):
+    return db.query(User).all()
